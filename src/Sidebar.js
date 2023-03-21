af4cc247b2a0ac6c6bca7e736ea1cbf1d8689b84
Chaos <tinywonk@yahoo.com>
2023-03-21 19:32:04 -0400

    Fixed server name truncate and style

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,39 +1,55 @@
 import { GiMagnifyingGlass, GiRingingBell } from "react-icons/gi";
 import { VscGear } from "react-icons/vsc";
 import PlayerIcon from './PlayerIcon'
+// import { useState } from "react";
+import { useRef } from "react";
 
 
 
 const Sidebar = () => {
+   
+    // const [servername, setServerName] = useState(true);
+    const servername = useRef(null);
+    const secondServer = useRef(null);
+    const thirdServer =  useRef(null);
+     
+    const onFile = () => {
+        servername.current.innerText = servername.current.innerText.slice(0, 28).toUpperCase() + "...";
+        secondServer.current.innerText = secondServer.current.innerText.slice(0, 28).toUpperCase() + "...";
+        thirdServer.current.innerText = thirdServer.current.innerText.slice(0, 28).toUpperCase() + "...";
+
+    };
+
     return (
         <div className="fixed items-center top-0 left-0 h-screen  w-44 m-0 flex flex-col bg-primaryColor text-white shadow-lg">
             <button className="h-28 w-28 mt-4 hover:w-32 ease-in-out duration-150">
                 <img src="./logo.svg" alt="" /> 
             </button>
-            <ul className="flex-col flex my-2 mx-auto text-primaryHighlight list-disc" >
+            <ul className="flex-col flex my-2 mx-auto text-primaryHighlight list-disc"  >
+                {/* SERVER ONE */}
                   <span className="serverStatus"></span>
-                <li className="gameRoom my-5 rounded-full list-disc group">
-                    <img alt="" src="exit.svg" className=" hidden w-auto h-6 relative left-[103px;] top-3 group-hover:block"  />
-                  <button className="serverName" id="serverOne"> Street Fighter Third Strike Champions of EX Layer  </button>
-                    <img alt=""  src="./mute.svg" className=" hidden w-auto h-6 relative left-8 top-16 group-hover:block" />
+                <li className="gameRoom my-5 rounded-full list-disc group"onLoad={onFile}>
+                    <img alt="" src="exit.svg" className=" hidden w-auto h-6 absolute left-[103px;] top-3 group-hover:block"  />
+                  <button  className="serverName"  id="serverOne" ref={servername}>Street Fighter III 3rd Strike:Fight For Japan </button>
+                    <img alt=""  src="./mute.svg" className=" hidden w-auto h-6 absolute left-[103px] top-16 group-hover:block" />
                     
                 </li>
           
 
                 {/* SERVER TWO */}
                 <span className="serverStatus"></span>
-                <li className="gameRoom my-5 rounded-full list-disc group">
-                    <img alt="" src="exit.svg" className=" hidden w-auto h-6 relative left-[103px;] top-3 group-hover:block"  />
-                    <button className="serverName line-clamp-3" id="serverTwo"> The Last Blade 2 / bakumatsu of crisis  </button>
-                    <img alt=""  src="./mute.svg" className=" hidden w-auto h-6 relative left-8 top-16 group-hover:block" />
+                <li className="gameRoom my-5 rounded-full list-disc group" onLoad={onFile}>
+                    <img alt="" src="exit.svg" className=" hidden w-auto h-6 absolute left-[103px;] top-3 group-hover:block"  />
+                    <button className="serverName" id="serverTwo" ref={secondServer} > The Last Blade 2 / bakumatsu Roman - Dai Ni Maku Gekku No Kenshi  </button>
+                    <img alt=""  src="./mute.svg" className=" hidden w-auto h-6 absolute left-[103px] top-16 group-hover:block" />
                     
                 </li>
                 {/* SERVER THREE */}
                 <span className="serverStatus"></span>
-                <li className="gameRoom my-5 rounded-full list-disc group">
-                    <img alt="" src="exit.svg" className=" hidden w-auto h-6 relative left-[103px;] top-3 group-hover:block"  />
-                  <button className="serverName line-clamp-3" id="serverThree">Marvel VS Capcom 2 New Age Of Heroes </button>
-                    <img alt=""  src="./mute.svg" className=" hidden w-auto h-6 relative left-8 top-16 group-hover:block" />
+                <li className="gameRoom my-5 rounded-full list-disc group" onLoad={onFile}>
+                    <img alt="" src="exit.svg" className=" hidden w-auto h-6 absolute left-[103px;] top-3 group-hover:block"  />
+                  <button className="serverName" id="serverThree" ref={thirdServer} >Rage Of Dragons (NGM -264?) </button>
+                    <img alt=""  src="./mute.svg" className=" hidden w-auto h-6 absolute left-[103px] top-16 group-hover:block" />
                     
                 </li>
                 
@@ -66,20 +82,7 @@ const SidebarIcon = ({icon}) => (
 
     
 )
-const serverOneText = document.getElementById('serverOne').innerText;
-const serverTwoText = document.getElementById('serverTwo').innerText;
-const serverThreeText = document.getElementById('serverThree').innerText;
 
-//Target the text that we want
-
-const newServerOne = serverOneText.slice(0, 32) + "...";
-const newServerTwo = serverTwoText.slice(0, 32) + "...";
-const newServerThree = serverThreeText.slice(0, 32) + "...";
-
-console.log(newServerOne);
-console.log(newServerTwo);
-console.log(newServerThree);
-document.getElementById('serverName').innerText = newText;
 
 export default Sidebar
 
